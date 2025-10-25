f4ac35f76b302998bb6d3e6b83472a507aa93d07
Vermus <166741723+phvlvck@users.noreply.github.com>
2025-10-26 00:48:54 +0300

    Remove 32-bit check and import from FILE.py
    Removed 32-bit architecture check and import.

--- FILE.py
+++ FILE.py
@@ -10,6 +10,3 @@ bit = platform.architecture()[0]
 if bit == '64bit':
  print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m64Bit Found')
  import FILE64
- elif bit == '32bit':
-  print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m32Bit Found')
-  import FILE32
